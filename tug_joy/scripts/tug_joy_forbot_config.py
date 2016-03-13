b6814d7f90e49c0683c8923b7a98972a4a8fb50d
Jakob Auer <jakob.auer@student.tugraz.at>
2016-03-13 21:53:13 +0100

    tug_joy: forbot config: added functinality to control arm in cartesian mode and to move arm back home

--- tug_joy/scripts/tug_joy_forbot_config.py
+++ tug_joy/scripts/tug_joy_forbot_config.py
@@ -7,6 +7,9 @@ some predefined callbacks.
 
 import actionlib
 from control_msgs.msg import GripperCommandAction, GripperCommandGoal, GripperCommand
+from controller_manager_msgs.srv import SwitchController, SwitchControllerRequest
+from geometry_msgs.msg import TwistStamped
+import moveit_commander
 import rospy
 from tug_joy_constants import *
 from tug_joy_base import Manager
@@ -27,7 +30,27 @@ All callback functions are defined in this section. Do not forget to create the 
 also be used to add or remove callbacks of the manager.
 """
 
+CONTROLLER_MANAGER_NS = "/controller_manager"
+SWITCH_CONTROLLERS_SERVICE = CONTROLLER_MANAGER_NS + "/switch_controller"
+MOVEIT_CONTROLLERS = ["arm_controller"]
+CARTESIAN_CONTROLLERS = ["cartesian_controller"]
+JOINT_VELOCITY_CONTROLLERS = ["arm_%d_joint_velocity_controller" % (i + 1) for i in range(6)]
+
 print "loading forbot config"
+
+rospy.wait_for_service(SWITCH_CONTROLLERS_SERVICE, 10.0)
+switch_controllers_service = rospy.ServiceProxy(SWITCH_CONTROLLERS_SERVICE, SwitchController)
+def switch_controllers(start_controllers, stop_controllers):
+    global switch_controllers_service
+    try:
+        resp = switch_controllers_service(start_controllers=start_controllers,
+                                          stop_controllers=stop_controllers,
+                                          strictness=SwitchControllerRequest.STRICT)
+    except rospy.ServiceException as e:
+        rospy.logerr("Error calling switch controllers service: %s", e)
+    else:
+        if not resp.ok:
+            rospy.logerr("Switch controllers service failed")
 
 # Cmd vel
 cmd_vel = CmdVel(actuator_linear_x=AXIS.STICK_AXIS_LEFT_VERTICAL,
@@ -77,6 +100,8 @@ def close_gripper(values_dict):
 def open_gripper(values_dict):
     call_gripper_action(0.11)
 
+
+
 arm_joints = [Callback(name='arm_1_joint', actuators=arm_1_joint.actuator, callback_fct=arm_1_joint.callback),
               Callback(name='arm_2_joint', actuators=arm_2_joint.actuator, callback_fct=arm_2_joint.callback),
               Callback(name='arm_3_joint', actuators=arm_3_joint.actuator, callback_fct=arm_3_joint.callback),
@@ -87,32 +112,69 @@ arm_joints = [Callback(name='arm_1_joint', actuators=arm_1_joint.actuator, callb
               Callback(name='gripper_open',  actuators=BUTTONS.CROSS_2_BUTTON_DOWN, callback_fct=open_gripper, cb_filtering=CB_FILTERING_PRESS),
               ]
 
+arm_cartesian_publisher = rospy.Publisher("/cartesian_controller/arm_cmd_vel", TwistStamped, queue_size=1)
+def move_arm_cartesian(values_dict):
+    global arm_cartesian_publisher
+    msg = TwistStamped()
+    msg.twist.linear.x = 0.1 * values_dict[AXIS.STICK_AXIS_LEFT_VERTICAL]
+    msg.twist.linear.y = -0.1 * values_dict[AXIS.STICK_AXIS_LEFT_HORIZONTAL]
+    msg.twist.linear.z = 0.1 * values_dict[AXIS.STICK_AXIS_RIGHT_VERTICAL]
+    arm_cartesian_publisher.publish(msg)
+
+arm_cartesian = [Callback(name='cartesian', actuators=[AXIS.STICK_AXIS_LEFT_HORIZONTAL,
+                                                       AXIS.STICK_AXIS_LEFT_VERTICAL,
+                                                       AXIS.STICK_AXIS_RIGHT_VERTICAL],
+                          callback_fct=move_arm_cartesian)]
+
+
+move_group = moveit_commander.MoveGroupCommander("lwa4p_arm")
+def arm_go_home_cb(values_dict):
+    global move_group
+    if values_dict[BUTTONS.SHOULDER_BUTTON_LOWER_LEFT] and values_dict[BUTTONS.SHOULDER_BUTTON_LOWER_RIGHT]:
+        move_group.clear_pose_targets()
+        move_group.set_named_target("home")
+        plan = move_group.plan()
+        if plan:
+            move_group.execute(plan)
+
+arm_go_home = Callback(name='arm_go_home', actuators=[BUTTONS.SHOULDER_BUTTON_LOWER_LEFT, BUTTONS.SHOULDER_BUTTON_LOWER_RIGHT],
+                       callback_fct=arm_go_home_cb, cb_filtering=CB_FILTERING_PRESS)
+
 
 def enable_disable_arm_cb(values_dict):
-    if values_dict[BUTTONS.SHOULDER_BUTTON_UPPER_LEFT]:
+    left_shoulder = values_dict[BUTTONS.SHOULDER_BUTTON_UPPER_LEFT]
+    right_shoulder = values_dict[BUTTONS.SHOULDER_BUTTON_UPPER_RIGHT]
+    if left_shoulder and not right_shoulder:
+        switch_controllers(JOINT_VELOCITY_CONTROLLERS, MOVEIT_CONTROLLERS + CARTESIAN_CONTROLLERS)
         Manager().add_callback_list(arm_joints)
+    elif right_shoulder and not left_shoulder:
+        switch_controllers(CARTESIAN_CONTROLLERS, MOVEIT_CONTROLLERS + JOINT_VELOCITY_CONTROLLERS)
+        Manager().add_callback_list(arm_cartesian)
     else:
         stop_arm(values_dict)
 
 
 def stop_arm(values_dict):
     Manager().remove_callback_list(arm_joints)
+    Manager().remove_callback_list(arm_cartesian)
     arm_1_joint.stop_cb(values_dict)
     arm_2_joint.stop_cb(values_dict)
     arm_3_joint.stop_cb(values_dict)
     arm_4_joint.stop_cb(values_dict)
     arm_5_joint.stop_cb(values_dict)
     arm_6_joint.stop_cb(values_dict)
+    switch_controllers(MOVEIT_CONTROLLERS, CARTESIAN_CONTROLLERS + JOINT_VELOCITY_CONTROLLERS)
 
-enable_arm = Callback('Arm_On', [BUTTONS.SHOULDER_BUTTON_UPPER_LEFT], enable_disable_arm_cb, CB_FILTERING_PRESS)
-disable_arm = Callback('Arm_Off', [BUTTONS.SHOULDER_BUTTON_UPPER_LEFT], enable_disable_arm_cb, CB_FILTERING_RELEASE)
+enable_arm = Callback('Arm_On', [BUTTONS.SHOULDER_BUTTON_UPPER_LEFT, BUTTONS.SHOULDER_BUTTON_UPPER_RIGHT], enable_disable_arm_cb, CB_FILTERING_PRESS)
+disable_arm = Callback('Arm_Off', [BUTTONS.SHOULDER_BUTTON_UPPER_LEFT, BUTTONS.SHOULDER_BUTTON_UPPER_RIGHT], enable_disable_arm_cb, CB_FILTERING_RELEASE)
 
 
 # switch between arm and robot
 def select_robot_cb(values_dict):
-    print "select_robot_cb"
+    print "Selected base"
     Manager().remove_callback(enable_arm)
     Manager().remove_callback(disable_arm)
+    Manager().remove_callback(arm_go_home)
     stop_arm(values_dict)
 
     Manager().add_callback(enable_cmd_vel)
@@ -120,13 +182,14 @@ def select_robot_cb(values_dict):
 
 
 def select_arm_cb(values_dict):
-    print "select_arm_cb"
+    print "Selected arm"
     Manager().remove_callback(enable_cmd_vel)
     Manager().remove_callback(disable_cmd_vel)
     stop_cmd_vel(values_dict)
 
     Manager().add_callback(enable_arm)
     Manager().add_callback(disable_arm)
+    Manager().add_callback(arm_go_home)
 
 
 
