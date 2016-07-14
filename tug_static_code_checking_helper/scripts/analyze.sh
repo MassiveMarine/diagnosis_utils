b0dfc13e82b207cd4ae636e26351b3f299c59a40
Clemens Muehlbacher <cmuehlba@ist.tugraz.at>
2016-07-14 16:10:58 +0200

    moved static code checking stuf into ci git

--- tug_static_code_checking_helper/scripts/analyze.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -z "$SOURCEMETERCPP_PATH" ]; then
-  echo "can't perform check if source meter as no path is set";
-else
-    $SOURCEMETERCPP_PATH/SourceMeterCPP -projectName=catkin_project -buildScript=build.sh -resultsDir=Results -externalSoftFilter=softfilter -resultsDir=/tmp;
-fi
--- tug_static_code_checking_helper/scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-the_path=$(echo $ROS_PACKAGE_PATH | awk -F: '{print $1}')
-source $the_path/../devel/setup.sh
-cd $the_path/../
-catkin_make
