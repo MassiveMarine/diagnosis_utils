fae294411995e55a615493e6aaad4d3dde8dbf8c
Alexander Buchegger <alexander.buchegger@student.tugraz.at>
2017-04-02 17:44:06 +0200

    tug_cfg: introduced ElementSpec to define XML structure

--- tug_cfg/python_src/tug_cfg/loader.py
+++ tug_cfg/python_src/tug_cfg/loader.py
@@ -1,5 +1,5 @@
 import re
-from tug_cfg import model
+from tug_cfg.model import Cfg, Param
 from xml.etree import ElementTree
 
 
@@ -13,12 +13,42 @@ class ErrorHandler(object):
     def handle_unknown_attribute(self, elem, name):
         print('Warning: unknown attribute "%s" in "%s" element; ignored' % (name, elem.tag))
 
+    def handle_invalid_attribute_value(self, elem, name, value):
+        print('Warning: attribute "%s" in "%s" element has invalid value; ignored' % (name, elem.tag))
+
     def handle_unexpected_text(self, elem):
         print('Warning: "%s" element contains text; ignored' % elem.tag)
 
 
+class ElementSpec(object):
+    def __init__(self, class_, text_only=False, attributes={}, children={}):
+        self.class_ = class_
+        self.text_only = text_only
+        self.attributes = attributes
+        self.children = children
+
+
 class Loader(object):
-    _NAME_RE = re.compile('^[a-z]+(-[a-z]+)*$')
+    _SPECS = dict(
+        cfg=ElementSpec(Cfg, attributes=dict(
+            extends=None,
+        ), children=dict(
+            param=ElementSpec(Param, attributes=dict(
+                name=re.compile('^[a-z][a-z0-9]*(_[a-z0-9]+)*$'),
+                type=re.compile('^(bool|double|int|str)$'),  # Only simple types for now
+                unit=None,
+                default=None,
+                description=None,
+                min=None,
+                max=None,
+                dynamic=re.compile('^(true|false)$'),
+                level=re.compile('^(0|[1-9][0-9]*)$'),
+                choices=None,
+                suggestions=None,
+                ignored=re.compile('^(true|false)$'),
+            ))
+        ))
+    )
 
     def __init__(self, error_handler=ErrorHandler()):
         self._error_handler = error_handler
@@ -29,38 +59,37 @@ class Loader(object):
         except Exception as e:
             return self._error_handler.handle_parse_error(e)
         else:
-            return self._process_elem(root_elem)
+            return self._process_elem(None, root_elem, self._SPECS)
 
-    def _process_elem(self, parent_object, elem):
-        if re.match(self._NAME_RE, elem.tag):
-            class_name = ''.join(s.capitalize() for s in elem.tag.split('-'))
-            if hasattr(model, class_name):
-                class_ = getattr(model, class_name)
-                if isinstance(class_, type):
-                    object_ = class_(self._error_handler)
-                    # Go through attributes:
-                    for k, v in elem.items():
-                        self._process_attribute(object_, elem, k, v)
-                    if elem.text is not None and not elem.text.isspace():
-                        if len(elem) == 0 and hasattr(object_, 'text'):
-                            object_.text = elem.text
-                        else:
-                            self._error_handler.handle_unexpected_text(elem)
-                    for child_elem in elem:
-                        child_object = self._process_elem(object_, child_elem)
-                        if child_elem.text is not None and not child_elem.text.isspace() and len(child_elem) == 0:
-                            self._process_attribute(child_elem.tag, child_elem.text)
-                        else:
-                            self._process_attribute(child_elem.tag, child_elem)
-                        if child_elem.tail is not None and not child_elem.tail.isspace():
-                            # Tail text is actually part of parent element, so show it as warning of parent element:
-                            print('Warning: "%s" element contains text; ignored' % elem.tag)
+    def _process_elem(self, parent_object, elem, specs):
+        if elem.tag in specs:
+            spec = specs[elem.tag]
+            object_ = spec.class_()
+            # Go through attributes:
+            for k, v in elem.items():
+                self._process_attribute(object_, elem, spec.attributes, k, v)
+            if elem.text is not None and not elem.text.isspace():
+                if len(elem) == 0 and hasattr(object_, 'text'):
+                    object_.text = elem.text
+                else:
+                    self._error_handler.handle_unexpected_text(elem)
+            for child_elem in elem:
+                child_object = self._process_elem(object_, child_elem)
+                if child_elem.text is not None and not child_elem.text.isspace() and len(child_elem) == 0:
+                    self._process_attribute(child_elem.tag, child_elem.text)
+                else:
+                    self._process_attribute(child_elem.tag, child_elem)
+                if child_elem.tail is not None and not child_elem.tail.isspace():
+                    # Tail text is actually part of parent element, so show it as warning of parent element:
+                    print('Warning: "%s" element contains text; ignored' % elem.tag)
         self._error_handler.handle_unknown_tag(elem)
 
-    def _process_attribute(self, object_, elem, k, v):
-        if re.match(self._NAME_RE, k):
-            attr_name = '_'.join(k.split('-'))
-            if hasattr(object_, attr_name):
-                setattr(object_, attr_name, v)
-                return
+    def _process_attribute(self, object_, elem, specs, k, v):
+        if k in specs:
+            spec = specs[k]
+            if spec is None or spec.match(v):
+                setattr(object_, k, v)
+            else:
+                self._error_handler.handle_invalid_attribute_value(elem, k, v)
+            return
         self._error_handler.handle_unknown_attribute(elem, k)
--- tug_cfg/python_src/tug_cfg/model.py
+++ tug_cfg/python_src/tug_cfg/model.py
@@ -6,32 +6,7 @@ class CfgComponent(object):
 
 class Cfg(CfgComponent):
     def __init__(self):
-        self._extends = None
-        self._params = []
-
-    @property
-    def extends(self):
-        return self._extends
-
-    @extends.setter
-    def extends(self, value):
-        if self._extends is not None:
-            raise Warning('Trying to set "extends" property more than once; ignored')
-        # TODO: parse and evaluate type
-        self._extends = value
-
-    @property
-    def param(self):
-        return self._params
-
-    @param.setter
-    def param(self, value):
-        if isinstance(value, ElementTree.Element):
-            p = Param()
-            p.init_from_xml(value)
-            self._params.append(p)
-        else:
-            raise Warning('Invalid contents for "param"; ignored')
+        self.extends = None
 
 
 class Param(CfgComponent):
@@ -49,10 +24,7 @@ class Param(CfgComponent):
         self.suggestions = None
         self.ignored = False
 
-    def init_from_xml(self, elem):
-        super(Param, self).init_from_xml(elem)
-        if self.name is None:
-            raise ValueError('"param" tag is lacking name')
-        elif not self.ignored:
-            if self.type is None or (self.type.isalpha() and self.default is None):
-                raise ValueError('"param" tag named "%s" is lacking required attributes' % self.name)
+
+class Description(CfgComponent):
+    def __init__(self):
+        self.text = None
